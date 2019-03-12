0e651290302857384f954a09127b90a44f819eeb
matt henley <mattbhenley@gmail.com>
2019-03-12 13:23:59 -0500

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -34,3 +34,27 @@ gulp.task('minify', function () {
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
 });
+
+// Compile Sass
+gulp.task('sass', function () {
+    gulp.src('src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Scripts
+gulp.task('scripts', function () {
+    gulp.src('src/js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['message', 'copyHtml', 'imageMin', 'sass', 'scripts']);
+
+gulp.task('watch', function () {
+    gulp.watch('src/js/*.js', ['scripts']);
+    gulp.watch('src/images/*', ['imageMin']);
+    gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch('src/*.html', ['copyHtml']);
+});
